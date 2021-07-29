4ab26726bda0a320317f9fd68c35c94ea41d763b
Anna <annaange55@gmail.com>
2021-07-29 18:23:17 +0300

    Added mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 const adaptive = document.getElementById('adaptive');
 const header = document.getElementById('header');
-const headerNone = document.querySelectorAll('header-none');
+const headerNone = document.querySelectorAll('.header-none');
 const switchMenu = () => {
     header.classList.toggle('bg-black');
     adaptive.classList.toggle('adaptive-checked');
+    for (let i = 0; i < headerNone.length; i++) {
+        headerNone[i].classList.toggle('header-none')
+    }
 }
 adaptive.addEventListener('click', switchMenu);
