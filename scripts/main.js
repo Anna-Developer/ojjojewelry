0624504ed79cbcab82d4ffde28453ad45ee3b6c6
Anna <annaange55@gmail.com>
2021-10-27 18:45:22 +0300

    added a very beautiful search input

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+'use strict'
+
+// menu
 const adaptive = document.getElementById('adaptive');
 const header = document.getElementById('header');
 const headerNone = document.querySelectorAll('.header-none');
@@ -9,3 +12,20 @@ const switchMenu = () => {
     }
 }
 adaptive.addEventListener('click', switchMenu);
+
+// search
+const search = document.getElementById('search');
+const searchIcon = document.getElementById('searchIcon');
+const searchInput = document.getElementById('searchInput');
+
+function switchSearchTools(event) {
+    if (event.target.id == 'searchIcon') {
+        search.innerHTML = `<input type="search" placeholder="Поиск" class="header__search" id="searchInput">`;
+    } else if (event.target.id == 'searchInput') {
+        return
+    } else {
+        search.innerHTML = `<img src="./img/search-icon.png" id="searchIcon">`;
+    }
+}
+
+document.addEventListener('click', switchSearchTools);
